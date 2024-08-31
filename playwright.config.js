b246edd0191efe1ebab6501349f89c24822fa8ce
Meenakshi Sundaram M <66931555+webdesigning4beginners@users.noreply.github.com>
2024-08-31 11:38:31 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    video: 'on',
+    video: 'off',
     actionTimeout: 60*1000,
     navigationTimeout: 60*1000,
     
